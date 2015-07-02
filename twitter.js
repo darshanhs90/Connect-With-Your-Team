4584e42427a9998a5e3cba67f2d6fcd50ef07cf1
Haridarshan HS <hsdars@gmail.com>
2015-07-02 11:37:49 -0500

    twitter api js

---
+++ twitter.js
@@ -0,0 +1,15 @@
+var Twitter = require('twitter');
+ 
+var client = new Twitter({
+    consumer_key: 'LmNp3JwAQZnuBr4SQFaM7UZG3',
+    consumer_secret: 'Xps6ziqIhZ0exAPoIAeyqj7myu7L78ZLHQDni67dzD9koJQTAD',
+    access_token_key: '151128859-F4Wk8KebqH4ZDwp8tMWY8PkoTQzfiEJrN1t2Knfc',
+    access_token_secret: 'czQre16YZKoC4Csi18gGufu8PxF733aL5VnzbhurlGvHw'
+});
+ 
+var params = {screen_name: 'nodejs'};
+client.get('statuses/user_timeline', params, function(error, tweets, response){
+  if (!error) {
+    console.log(tweets);
+  }
+});
