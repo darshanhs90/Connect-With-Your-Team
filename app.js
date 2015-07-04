eeb55d1c6a7876d9637cc320d9da559ee86e9ce5
Haridarshan HS <hsdars@gmail.com>
2015-07-04 16:48:44 -0500

    app.js modified,has errors still

--- app.js
+++ app.js
@@ -23,9 +23,28 @@ var sendgrid = require('sendgrid')('hsdars', 'Password90-');
 var accountSid = 'AC07275e4294f1b0d42623c3ec9559911e';
 var authToken = '650d049a9bd99323fb899ce4b9e84fcc';
 var clientTwilio = require('twilio')(accountSid, authToken);
+var Twit = require('twit');
 
+var T = new Twit({
+   consumer_key: 'LmNp3JwAQZnuBr4SQFaM7UZG3',
+    consumer_secret: 'Xps6ziqIhZ0exAPoIAeyqj7myu7L78ZLHQDni67dzD9koJQTAD',
+    access_token: '151128859-F4Wk8KebqH4ZDwp8tMWY8PkoTQzfiEJrN1t2Knfc',
+    access_token_secret: 'czQre16YZKoC4Csi18gGufu8PxF733aL5VnzbhurlGvHw'
+})
 
+var OAuth = require('oauth').OAuth
+  , oauth = new OAuth(
+      "https://api.twitter.com/oauth/request_token",
+      "https://api.twitter.com/oauth/access_token",
+      "LmNp3JwAQZnuBr4SQFaM7UZG3",
+      "Xps6ziqIhZ0exAPoIAeyqj7myu7L78ZLHQDni67dzD9koJQTAD",
+      "1.0",
+      "oob",
+      "HMAC-SHA1"
+    );
+  var xoauth;
 
+var Bing = require('node-bing-api')({ accKey: "l11l8D4FBj6XkyHh3NzeMINbdY+s19eUoxrRgvgQQgQ" });
 
 // cfenv provides access to your Cloud Foundry environment
 // for more info, see: https://www.npmjs.com/package/cfenv
@@ -51,10 +70,191 @@ app.listen(appEnv.port, appEnv.bind, function() {
 
 
 
+//login modules start
+app.get('/auth/twitter', function(req, res) {
+ 
+  oauth.getOAuthRequestToken(function(error, oauth_token, oauth_token_secret, results) {
+    if (error) {
+      console.log(error);
+      res.send("Authentication Failed!");
+    }
+    else {
+      xoauth = {
+        token: oauth_token,
+        token_secret: oauth_token_secret
+      };
+      console.log(xoauth);
+      res.redirect('https://twitter.com/oauth/authenticate?oauth_token='+oauth_token)
+    }
+  });
+ 
+});
 
 
-//analyse company score in twitter passing tweet to ibm sentiment analyser
-app.use('/twitterCompanySentiment', function(reqst, respns) {
+  app.get('/auth/twitter/callback', function(req, res, next) {
+ 
+  if (xoauth) {
+    xoauth.verifier = 5744204;//req.query.oauth_verifier;
+    var oauth_data = xoauth;
+ 
+    oauth.getOAuthAccessToken(
+      oauth_data.token,
+      oauth_data.token_secret,
+      oauth_data.verifier,
+      function(error, oauth_access_token, oauth_access_token_secret, results) {
+        if (error) {
+          console.log(error);
+          res.send("Authentication Failure!");
+        }
+        else {
+          xoauth.access_token = oauth_access_token;
+          xoauth.access_token_secret = oauth_access_token_secret;
+          console.log(results, req.session.oauth);
+          console.log('token');
+
+          console.log(oauth_access_token);
+                    console.log('token secret');
+            console.log(oauth_access_token_secret);
+
+
+
+          res.end("Authentication Successful");
+          // res.redirect('/'); // You might actually want to redirect!
+        }
+      }
+    );
+  }
+  else {
+    res.redirect('/login'); // Redirect to login page
+  }
+ 
+});
+
+//login modules end
+
+//********************//
+//stream tweets based on location and topic
+
+var sanFrancisco = [ '-122.75', '36.8', '-121.75', '37.8' ]
+
+var stream = T.stream('statuses/filter', {locations:sanFrancisco})
+
+stream.on('tweet', function (tweet) {
+    var x=tweet.text.toString().toLowerCase();
+    if(x.indexOf("ca") > -1){
+        console.log('*****************');
+  console.log(tweet);
+  console.log(tweet.text);
+        console.log('*****************');
+  }
+  else{
+    //console.log(tweet.text);
+  }
+})
+
+//****************//
+//stream based on topic alone
+client.stream('statuses/filter', {track: 'federer'}, function(stream) {
+  stream.on('data', function(tweet) {
+    console.log(tweet.text);
+    console.log(tweet.location);
+    console.log(tweet);
+
+     alchemy.keywords(tweet.text, {}, function(err, response) {
+        if (err) throw err;
+
+        // See http://www.alchemyapi.com/api/keyword/htmlc.html for format of returned object
+        var keywords = response.keywords;
+        console.log('keywords');
+            console.log(keywords);
+         // Do something with data
+        });
+         });
+ 
+  stream.on('error', function(error) {
+    console.log(error);
+    throw error;
+  });
+});
+
+//****************//
+//get followers list
+var params = {};
+client.get('followers/list', params, function(error, tweets, response){
+  if (!error) {
+    console.log(tweets);
+  }
+});
+
+//**************//
+//get keywords
+alchemy.keywords('', {}, function(err, response) {
+  if (err) throw err;
+
+  // See http://www.alchemyapi.com/api/keyword/htmlc.html for format of returned object
+  var keywords = response.keywords;
+
+  // Do something with data
+});
+
+//*************//
+//Bing search
+
+//news search
+// Bing.news("RCB", {
+//     top: 10,  // Number of results (max 50) 
+//     skip: 3,   // Skip first 3 results 
+//     newssortby: "Date", //Choices are: Date, Relevance 
+//     newscategory: "rt_Sports" // Choices are: 
+//                                 //   rt_Business 
+//                                 //   rt_Entertainment 
+//                                 //   rt_Health 
+//                                 //   rt_Politics 
+//                                 //   rt_Sports 
+//                                 //   rt_US 
+//                                 //   rt_World 
+//                                 //   rt_ScienceAndTechnology 
+//   }, function(error, res, body){
+//     console.log(body.d.results[3]);
+//   });
+
+//composite search
+// Bing.composite("xbox", {
+//     top: 10,  // Number of results (max 50) 
+//     skip: 3,   // Skip first 3 results 
+//     sources: "web+news", //Choises are web+image+video+news+spell 
+//     newssortby: "Date" //Choices are Date, Relevance 
+//   }, function(error, res, body){
+//     console.log(body);
+//   });
+
+//video search
+// Bing.video("monkey vs frog", {
+//     top: 10,  // Number of results (max 50) 
+//     skip: 3,   // Skip first 3 result 
+//     videofilters: {
+//       duration: 'short',
+//       resolution: 'high'
+//     }
+//   }, function(error, res, body){
+//     console.log(body);
+//   });
+
+
+//images search
+// Bing.images("Ninja Turtles", {skip: 50}, function(error, res, body){
+//   console.log(body);
+// });  
+
+//**********************//
+
+
+
+
+
+
+
+app.use('/twitterSentiment', function(reqst, respns) {
 
     //get companyname from request
     var companyName=reqst.query.companyName;
@@ -91,7 +291,6 @@ app.use('/twitterCompanySentiment', function(reqst, respns) {
                     respns.end(total.toString());
                 }
                 }
-                //}
             });
             });
         
@@ -99,20 +298,6 @@ app.use('/twitterCompanySentiment', function(reqst, respns) {
 });
 
 app.get('/twitterInsight',function(reqst,respns){
-var val=reqst.query.val;
-http.get('http://nexruiter.webuda.com/retrieve.php',
-        function(response){
-            var body = '';
-            response.on('data', function(d) {
-                body += d;
-            });
-            response.on('end', function() {
-
-               
-               // console.log(body);
-                
-                 var parsed = JSON.parse(body);
-                 var textval=(parsed[val].rec_data);
 
 
  alchemy.sentiment(textval, {}, function(err, response) {
@@ -124,162 +309,11 @@ http.get('http://nexruiter.webuda.com/retrieve.php',
                 //res.send(asd);
                 respns.end(JSON.stringify(sentiment));
             });
-});
-        });
-
-});
-
-
-//company info
-app.get('/companyInfo', function(reqst, respns) {
-    //company website
-    var companyName=reqst.query.companyName;
-    console.log(companyName);
-    https.get('https://api.fullcontact.com/v2/company/lookup.json?domain='+companyName+'.com&apiKey=f6e2b2695278badc',
-        function(response) {
-            var body = '';
-            response.on('data', function(d) {
-                body += d;
-            });
-            response.on('end', function() {
-
-                // Data reception is done, do whatever with it!
-                var parsed = JSON.parse(body);
-                console.log('linkedin');
-                respns.send(parsed);
-            });
-
-        });
 
 });
 
 
 
-//get student info
-app.get('/personInfo', function(reqst, respns) {
-    //person email id  
-    var email=reqst.query.email;
-    console.log(email);
-    https.get('https://api.fullcontact.com/v2/person.json?email='+email+'&apiKey=f6e2b2695278badc',
-        function(response) {
-            var body = '';
-            response.on('data', function(d) {
-                body += d;
-            });
-            response.on('end', function() {
-
-                // Data reception is done, do whatever with it!
-                var parsed = JSON.parse(body);
-                console.log(parsed);
-                respns.send(parsed);
-            });
-
-        });
-
-});
-
-//get student info
-app.get('/personalityInsights', function(reqst, respns) {
-   var val=reqst.query.val;
-
-http.get('http://nexruiter.webuda.com/retrieve.php',
-        function(response){
-            var body = '';
-            response.on('data', function(d) {
-                body += d;
-            });
-            response.on('end', function() {
-
-               
-               // console.log(body);
-                
-                 var parsed = JSON.parse(body);
-                 var textval=(parsed[val].rec_data);
-                 
-
-   var personality_insights = watson.personality_insights({
-                                                "username": "51397aaa-2786-4342-9cb7-40f1225de1a7",
-                                                "password": "ufQQJaVROpjw",
-                                                version: 'v2'
-                                            });
-
-                                            personality_insights.profile({
-                                                    text: textval
-                                                },
-                                                function(err, response) {
-                                                    if (err)
-                                                        console.log('error:', err);
-                                                    else{
-                                                        var output=(JSON.stringify(response, null, 2));
-                                                        respns.end(output);
-                                                    }
-                                                });
-        });
-    });
-
-
-
-});
-
-
-//get student info
-app.get('/getCompInfo', function(reqst, respns) {
-    var companyName=reqst.query.companyName;
-    console.log(companyName);
-    var tickrsymbol='';
-//get tickr symbol
-http.get('http://d.yimg.com/autoc.finance.yahoo.com/autoc?query='+companyName+'&callback=YAHOO.Finance.SymbolSuggest.ssCallback',
-        function(response){
-            var body = '';
-            response.on('data', function(d) {
-                body += d;
-            });
-            response.on('end', function() {
-
-                // Data reception is done, do whatever with it!
-                //var parsed = JSON.parse(body);
-                console.log(body);
-                data=body
-    var string=data.substring(39);
-    string=string.split('').reverse().join('');
-    string=string.substring(1);
-    //console.log(string);
-    string=string.split('').reverse().join('');
-    string=(JSON.parse(string));
-    console.log(string);
-    if(string.ResultSet.Result.length==0)
-        respns.end('Invalid Tickr Symbol');
-    else{
-        companyName=(string.ResultSet.Result[0].symbol);
-
-
-
-https.get('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20=%22' + companyName + '%22&format=json&env=http%3A%2F%2Fdatatables.org%2Falltables.env'
-    , function (response) {
-        var rawData = '';
-        
-        response.on('data', function(chunk) {
-            rawData += chunk;
-            console.log(rawData);
-        });
-
-        response.on('end', function() {
-                // parse as JSON
- 
-              respns.end(rawData);
-
-            });
-                
-            
-        });
-
-    }
-});
-
-});
-
-
-});
 
 
 
